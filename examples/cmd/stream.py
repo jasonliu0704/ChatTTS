babcb207660712a91b96410ed59cff490601b597
proudnotice <jasonliu0704@gmail.com>
2024-10-14 11:20:55 +0800

    up

--- examples/cmd/stream.py
+++ examples/cmd/stream.py
@@ -77,6 +77,7 @@ class ChatStreamer:
         history_stream_wav = None
         article_streamwavs = None
         for stream_wav in streamchat:
+            print(f"stream_wav: {stream_wav}")
             print(np.abs(stream_wav).max(axis=1))
             n_texts = len(stream_wav)
             n_valid_texts = (np.abs(stream_wav).max(axis=1) > 1e-6).sum()
